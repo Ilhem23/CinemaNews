dd2bae5a11350c55ddc07833b5fc39863278ed6c
Aissaoui <ei_aissaoui@esi.dz>
2018-04-19 03:11:08 +0200

    Initial Version

--- app/src/main/java/com/example/aissaouiilhem/cinemanews/Main2Activity.kt
+++ app/src/main/java/com/example/aissaouiilhem/cinemanews/Main2Activity.kt
@@ -3,7 +3,9 @@ package com.example.aissaouiilhem.cinemanews
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.design.widget.TabLayout
 import android.support.v4.view.GravityCompat
+import android.support.v4.view.ViewPager
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
@@ -11,13 +13,23 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main2.*
 import kotlinx.android.synthetic.main.app_bar_main2.*
 
-class Main2Activity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import java.util.ArrayList
 
+class Main2Activity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private var tabLayout: TabLayout? = null
+    var viewPager: ViewPager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         setSupportActionBar(toolbar)
+        viewPager = findViewById(R.id.viewpager)
+        setupViewPager(viewPager!!)
 
+        tabLayout = findViewById(R.id.tabs)
+        tabLayout!!.setupWithViewPager(viewPager)
 
 
         val toggle = ActionBarDrawerToggle(
@@ -27,6 +39,36 @@ class Main2Activity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
 
         nav_view.setNavigationItemSelectedListener(this)
 
+    }
+
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(AccueilFragment(), "Accueil")
+        adapter.addFragment(FilmFragment(), "Films")
+        adapter.addFragment(SerieFragment(), "Série")
+        viewPager.adapter = adapter
+    }
+
+    internal inner class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+        private val mFragmentList = ArrayList<Fragment>()
+        private val mFragmentTitleList = ArrayList<String>()
+
+        override fun getItem(position: Int): Fragment {
+            return mFragmentList[position]
+        }
+
+        override fun getCount(): Int {
+            return mFragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            mFragmentList.add(fragment)
+            mFragmentTitleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return mFragmentTitleList[position]
+        }
     }
 
     override fun onBackPressed() {
@@ -48,7 +90,7 @@ class Main2Activity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
-            R.id.action_settings -> return true
+            R.id.action_search -> return true
             else -> return super.onOptionsItemSelected(item)
         }
     }
