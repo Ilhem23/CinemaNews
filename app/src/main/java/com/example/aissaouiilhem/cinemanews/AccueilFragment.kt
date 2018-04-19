a00fe116d3445fdc172b45144447484a38021a1f
Aissaoui <ei_aissaoui@esi.dz>
2018-04-19 05:21:57 +0200

    Initial Version 2

--- app/src/main/java/com/example/aissaouiilhem/cinemanews/AccueilFragment.kt
+++ app/src/main/java/com/example/aissaouiilhem/cinemanews/AccueilFragment.kt
@@ -3,24 +3,37 @@ package com.example.aissaouiilhem.cinemanews
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.app_bar_main2.*
+import kotlinx.android.synthetic.main.fragment_accueil.*
 
 
 /**
  * A simple [Fragment] subclass.
  */
 class AccueilFragment : Fragment() {
-
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater!!.inflate(R.layout.fragment_accueil, container, false)
+
+        layoutManager = LinearLayoutManager(this.context)
+        recycler_view.layoutManager = layoutManager
+
+        adapter = RecyclerAdapter()
+        recycler_view.adapter = adapter
+
+
     }
 
 }// Required empty public constructor
