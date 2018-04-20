763fd794f492c047b7840865ed692dfa1fd3b212
Aissaoui <ei_aissaoui@esi.dz>
2018-04-20 20:57:50 +0200

    saison et episode layout

--- app/src/main/java/com/example/aissaouiilhem/cinemanews/Saison.kt
+++ app/src/main/java/com/example/aissaouiilhem/cinemanews/Saison.kt
@@ -6,24 +6,25 @@ import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import android.view.View
 import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_saison.*
 import kotlinx.android.synthetic.main.activity_serie_description.*
 
-class serieDescription : AppCompatActivity() {
+class Saison : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_serie_description)
+        setContentView(R.layout.activity_saison)
 
-        val toolbar = findViewById<View>(R.id.toolbar3) as Toolbar
+        val toolbar = findViewById<View>(R.id.toolbar4) as Toolbar
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
 
         toolbar.setNavigationOnClickListener {
-            val intent = Intent(this, Main2Activity::class.java )
+            val intent = Intent(this, serieDescription::class.java )
             startActivity(intent) }
-        val btn: ImageButton = sale1
-        setEvent(btn,Saison::class.java)
+        val btn: ImageButton = epi1
+        setEvent(btn,Episode::class.java)
     }
     private fun setEvent(btn : ImageButton, cls: Class<*>) {
 
@@ -33,5 +34,4 @@ class serieDescription : AppCompatActivity() {
         })
 
     }
-
 }
